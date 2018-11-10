319a7efa563c58a0daf924ff232bd7985d968548
IKillerwolfI <killerwolf.no1@gmail.com>
2018-11-10 07:08:04 +0100

    ADS fixes
    -Fixed the insurgency sights (545 module was used)

--- hooks/playertweakdata.lua
+++ hooks/playertweakdata.lua
@@ -3,7 +3,8 @@ Hooks:PostHook( PlayerTweakData, "init", "AKEXTRAModStanceTweakData", function(s
 --AK
 -- ****** sights
 	self.stances.heffy_ins_ads = deep_clone(self.stances.flint)
-	self.stances.heffy_ins_ads.steelsight.shoulders.translation = Vector3(-10.247, -5.304, 5.15)
+	self.stances.heffy_ins_ads.steelsight.shoulders.translation = Vector3(-10.247, -5.304, 4.95) --5.15
+	self.stances.heffy_ins_ads.steelsight.shoulders.rotation = Rotation(0, -0.25, 0) 
 
 --Warrior 
 	self.stances.warrior_ads = deep_clone(self.stances.flint)
