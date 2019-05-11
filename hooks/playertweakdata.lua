b8fea6fe5ca2f47cb7b75e620856cea9ec582f73
IKillerwolfI <killerwolf.no1@gmail.com>
2019-05-11 21:12:54 +0200

    Sound preparation/Playertweak changes
    -Push your fuggin update >>>:(

--- hooks/playertweakdata.lua
+++ hooks/playertweakdata.lua
@@ -1,11 +1,6 @@
 Hooks:PostHook( PlayerTweakData, "init", "AKEXTRAModStanceTweakData", function(self)
 
 --AK
--- ****** sights
-	self.stances.heffy_ins_ads = deep_clone(self.stances.flint)
-	self.stances.heffy_ins_ads.steelsight.shoulders.translation = Vector3(-10.247, -5.304, 4.95) --5.15
-	self.stances.heffy_ins_ads.steelsight.shoulders.rotation = Rotation(0, -0.25, 0) 
-
 --Warrior 
 	self.stances.warrior_ads = deep_clone(self.stances.flint)
 	local ironsight_translation = Vector3(0, -13, -0.618)
@@ -13,8 +8,8 @@ Hooks:PostHook( PlayerTweakData, "init", "AKEXTRAModStanceTweakData", function(s
 
 --GL25
 	self.stances.gl25_gl = deep_clone(self.stances.contraband)
-	local ironsight_translation = Vector3(0, -6, 1.56)
-	self.stances.gl25_gl.steelsight.shoulders.translation = self.stances.gl25_gl.steelsight.shoulders.translation + ironsight_translation 
+	self.stances.gl25_gl.steelsight.shoulders.translation = Vector3(-10.49, -4.4, 2.75) 
+	self.stances.gl25_gl.steelsight.shoulders.rotation = Rotation(0.05, 0.05, 0) 
 
 --RPK
 	self.stances.heffy_bo1_ads = deep_clone(self.stances.flint)
